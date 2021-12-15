0f1006a8d272cd5f802be12b68794724d5de4636
强利飞 <878568782@qq.com>
2021-12-15 17:58:14 +0800

    Initial commit

---
+++ app/src/main/java/com/bksx/mobile/terminal/ijkplayerdemo/MainActivity.java
@@ -0,0 +1,25 @@
+package com.bksx.mobile.terminal.ijkplayerdemo;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+
+import com.bksx.mobile.terminal.ijkplayerdemo.databinding.ActivityMainBinding;
+
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
